8479c53dc014d8a2ff1eb5a0709022f1f4fe321d
Sawyer Cherry <sawyeracherry@gmail.com>
2020-09-08 19:59:22 -0500

    first commit

---
+++ functions.py
@@ -0,0 +1,33 @@
+def area(width, height):
+    result = width * height
+    return result
+
+result = area(5, 6)
+print(result)
+
+result = area(9, 6)
+print(result)
+result = area(6, 3)
+print(result)
+
+def subtract(num1, num2):
+    result = num1 - num2
+    return result
+
+result = subtract(73, 8)
+print(result)
+result = subtract(6, 5)
+print(result)
+
+def divide(number_one, number_two):
+    result = number_one / number_two
+    return result
+
+def add(num1, num2):
+    result = num1 + num2
+    return result
+
+result = add(12, 4)
+print(result)
+result = add(15, 14)
+print(result)
